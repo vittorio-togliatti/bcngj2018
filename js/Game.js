bb134960ba5908c7df5c957c65d3c94a07f9c452
Pau <pau.kehos@gmail.com>
2018-01-27 03:13:39 +0100

    Updated game coordinates synchronization

--- js/Game.js
+++ js/Game.js
@@ -10,27 +10,17 @@ SideScroller.Game.prototype = {
   preload: function(){
  
       //this.game.time.advancedTiming = true;
-    this.load.image('background', 'img/debug-grid-1920x1920.png');
+    this.load.image('background', 'img/debug-grid.png');
     this.load.image('player','img/phaser-dude.png');
     this.load.image('casa','img/casa.png');
     this.load.image('calle','img/calle-dude.png');
     },
  
   create: function() {
-     
       
-      getJsonSync().then(function(data) {
-                            console.log(data);
-                        });
-      
-      postJsonSync("{'persona':'300,200'}").then(function(result) {
-                            console.log(result);
-                        });
-      
-      
-    this.game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    this.game.add.tileSprite(0, 0, 2400, 1800, 'background');
 
-    this.game.world.setBounds(0, 0, 1920, 1920);
+    this.game.world.setBounds(0, 0, 2400, 1800);
 
     this.game.physics.startSystem(Phaser.Physics.P2JS);
     //this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -54,9 +44,6 @@ SideScroller.Game.prototype = {
     player = this.game.add.sprite(this.game.world.centerX, this.world.centerY, 'player');
       
     this.game.physics.p2.enable(player);
-      
-      
-      
 
       
     this.game.physics.p2.enable(player);
@@ -72,6 +59,8 @@ SideScroller.Game.prototype = {
     this.game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
       
       
+    // SYncronize with the other instance
+    this.game.time.events.loop(Phaser.Timer.SECOND * 2, syncronize, this, player.body.sprite.position);
    
  }, 
  
@@ -101,11 +90,22 @@ SideScroller.Game.prototype = {
   render: function()
  
     {
- 
-              this.game.debug.cameraInfo(this.game.camera, 32, 32);
-        
+        this.game.debug.cameraInfo(this.game.camera, 32, 32);   
     }
     
     //functions
     
 };
+
+function syncronize( playerPosition ) {
+    
+    getJsonSync().then(function(data) {
+        console.log(data);
+    });
+    
+    var horizontalTile = parseInt(playerPosition.x / 60);
+    var verticalTile = parseInt(playerPosition.y / 60);
+    postJsonSync("{'persona': '" + horizontalTile + "," + verticalTile + "'}").then(function(result) {
+        console.log(result);
+    });
+}
