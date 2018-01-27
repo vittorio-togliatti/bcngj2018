244f875aea6b65459c8c243769b4b5ff99f0a3f1
isidro84 <battusai2004@msn.com>
2018-01-27 03:48:06 +0100

    merge

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,23 @@
 function pixelsToTile(x,y,tileHeight,tileWidth,screenHeight, screenWidth){
     var tilePosition = {x,y};
     
+    let numTancho = 0.0,
+        numTalto  = 0.0;
+    
+    numTancho = screenWidth / tileWidth;
+    numTalto  = screenHeight / tileHeight;
+    
+    position.x = Math.floor(x / numTancho);
+    position.y = Math.floor(y / numTalto);
+    
     return position;
 }
 
 function tileToPixels(tileX,tileY,tileHeight,tileWidth,screenHeight, screenWidth){
     var  pixelPosition = {x,y};
+    
+    pixelPosition.x = tileX * tileWidth;
+    pixelPosition.y = tileY * tileHeight;
     
     return pixelPosition;
 }
